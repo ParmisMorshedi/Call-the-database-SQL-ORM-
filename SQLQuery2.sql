ca2c14240aaea185a6216e9804a6b5e9ad56d539
ParmisMorshedi <146171609+ParmisMorshedi@users.noreply.github.com>
2023-11-07 15:17:30 +0100

    Delete SQLQuery2.sql

--- SQLQuery2.sql
+++
@@ -1,8 +0,0 @@
-SELECT Customers.CustomerID,  Count(Orders.OrderID) As TotalOrders 
-FROM Customers 
-Join Orders ON Customers.CustomerID = Orders.CustomerID
-Group By Customers.CustomerID
-ORDER BY totalOrders DESC;
-
-
-
