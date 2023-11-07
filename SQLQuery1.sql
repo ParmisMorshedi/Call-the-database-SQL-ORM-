352a4e983437cf25d0a4421f5fc54dfdd1432f1c
ParmisMorshedi <146171609+ParmisMorshedi@users.noreply.github.com>
2023-11-07 15:17:08 +0100

    Delete SQLQuery1.sql

--- SQLQuery1.sql
+++
@@ -1,7 +0,0 @@
-Select  Products.ProductName, Products.UnitPrice, Categories.CategoryName From Products 
-join Categories on Products.CategoryID= Categories.CategoryID
-ORDER BY Categories.CategoryName, Products.ProductName  ;
-
-
-
-
