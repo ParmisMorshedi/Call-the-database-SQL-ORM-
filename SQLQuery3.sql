b9048a1299e23fc5cea77734d844ff45cc4db708
ParmisMorshedi <146171609+ParmisMorshedi@users.noreply.github.com>
2023-11-07 15:17:40 +0100

    Delete SQLQuery3.sql

--- SQLQuery3.sql
+++
@@ -1,3 +0,0 @@
-SELECT * FROM Employees
-JOIN EmployeeTerritories ON Employees.EmployeeID = EmployeeTerritories.EmployeeID
-JOIN Territories ON EmployeeTerritories.TerritoryID= Territories.TerritoryID
